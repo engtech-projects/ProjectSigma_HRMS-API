fc2d56254a9728af2eae6d6c4e8bbaeac3e19d81
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-01-10 10:44:44 +0800

    build department api

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,8 @@
 
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use Illuminate\Database\Eloquent\softDeletes;
+use App\Http\Controllers\DepartmentController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +18,7 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+Route::resource('departments', DepartmentController::class);
+       
+
