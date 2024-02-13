05f2d279ce825b2d32b9cc7394e6f19bdc5e6a84
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-02-13 10:51:11 +0800

    Update UsersSeeder.php

--- database/seeders/UsersSeeder.php
+++ database/seeders/UsersSeeder.php
@@ -14,12 +14,44 @@ class UsersSeeder extends Seeder
      */
     public function run(): void
     {
-        DB::table('users')->insert([
-            'id' => 1,
-            'name' => "admin",
-            'email' => 'admin@gmail.com',
-            'password' => Hash::make('!onetimepassword'),
-            'accessibilities' => '[1]'
-        ]);
+        DB::table('users')->insert(
+            [
+                [
+                    'id' => 1,
+                    'name' => "admin",
+                    'email' => 'admin@gmail.com',
+                    'password' => Hash::make('!onetimepassword'),
+                    'accessibilities' => '[1]'
+                ],
+                [
+                    'id' => 2,
+                    'name' => "Jermily C. Mozo",
+                    'email' => 'jermilymozo@gmail.com',
+                    'password' => Hash::make('!onetimepassword'),
+                    'accessibilities' => '[1]'
+                ],
+                [
+                    'id' => 3,
+                    'name' => "ENGR. Richie C. Dalauta",
+                    'email' => 'richiedalauta@gmail.com',
+                    'password' => Hash::make('!onetimepassword'),
+                    'accessibilities' => '[1]'
+                ],
+                [
+                    'id' => 4,
+                    'name' => "ENGR. Dionision Jonas A. Rodes",
+                    'email' => 'dionisionrodes@gmail.com',
+                    'password' => Hash::make('!onetimepassword'),
+                    'accessibilities' => '[1]'
+                ],
+                [
+                    'id' => 5,
+                    'name' => "ENGR. Angel A. Abrau",
+                    'email' => 'angelabrau@gmail.com',
+                    'password' => Hash::make('!onetimepassword'),
+                    'accessibilities' => '[1]'
+                ],
+            ]
+        );
     }
 }
