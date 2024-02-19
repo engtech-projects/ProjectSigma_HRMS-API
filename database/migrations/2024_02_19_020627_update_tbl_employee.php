897352c50a546a8824d2143e4a02dd123a0f597f
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-02-19 16:19:11 +0800

    Update 2024_02_19_020627_update_tbl_employee.php

--- database/migrations/2024_02_19_020627_update_tbl_employee.php
+++ database/migrations/2024_02_19_020627_update_tbl_employee.php
@@ -9,13 +9,36 @@ return new class extends Migration
     /**
      * Run the migrations.
      */
+
     public function up(): void
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->date('date_of_birth')->nullable()->change();
-            $table->date('spouse_datebirth')->nullable()->change();
+            $table->string('nick_name')->nullable()->change();
+            $table->string('citizenship')->nullable()->change();
+            $table->string('blood_type')->nullable()->change();
+            $table->string('civil_status')->nullable()->change();
             $table->date('date_of_marriage')->nullable()->change();
+            $table->string('telephone_number')->nullable()->change();
+            $table->string('mobile_number')->nullable()->change();
             $table->string('email')->nullable()->change();
+            $table->string('religion')->nullable()->change();
+            $table->string('pre_street')->nullable()->change();
+            $table->string('pre_brgy')->nullable()->change();
+            $table->string('pre_city')->nullable()->change();
+            $table->string('pre_zip')->nullable()->change();
+            $table->string('pre_province')->nullable()->change();
+            $table->string('per_street')->nullable()->change();
+            $table->string('per_brgy')->nullable()->change();
+            $table->string('per_city')->nullable()->change();
+            $table->string('per_zip')->nullable()->change();
+            $table->string('per_province')->nullable()->change();
+            $table->string('father_name')->nullable()->change();
+            $table->string('mother_name')->nullable()->change();
+            $table->string('spouse_name')->nullable()->change();
+            $table->date('spouse_datebirth')->nullable()->change();
+            $table->string('spouse_occupation')->nullable()->change();
+            $table->string('spouse_contact_no')->nullable()->change();
+            $table->string('childrens')->nullable()->change();
             $table->string('weight')->nullable();
             $table->string('height')->nullable();
         });
@@ -27,10 +50,34 @@ return new class extends Migration
     public function down(): void
     {
         Schema::table('employees', function (Blueprint $table) {
-            $table->date('date_of_birth')->nullable(false)->change();
-            $table->date('spouse_datebirth')->nullable(false)->change();
+            $table->string('nick_name')->nullable(false)->change();
+            $table->string('citizenship')->nullable(false)->change();
+            $table->string('blood_type')->nullable(false)->change();
+            $table->string('civil_status')->nullable(false)->change();
             $table->date('date_of_marriage')->nullable(false)->change();
-            $table->date('email')->nullable(false)->change();
+            $table->string('telephone_number')->nullable(false)->change();
+            $table->string('mobile_number')->nullable(false)->change();
+            $table->string('email')->nullable(false)->change();
+            $table->string('religion')->nullable(false)->change();
+            $table->string('pre_street')->nullable(false)->change();
+            $table->string('pre_brgy')->nullable(false)->change();
+            $table->string('pre_city')->nullable(false)->change();
+            $table->string('pre_zip')->nullable(false)->change();
+            $table->string('pre_province')->nullable(false)->change();
+            $table->string('per_street')->nullable(false)->change();
+            $table->string('per_brgy')->nullable(false)->change();
+            $table->string('per_city')->nullable(false)->change();
+            $table->string('per_zip')->nullable(false)->change();
+            $table->string('per_province')->nullable(false)->change();
+            $table->string('father_name')->nullable(false)->change();
+            $table->string('mother_name')->nullable(false)->change();
+            $table->string('spouse_name')->nullable(false)->change();
+            $table->date('spouse_datebirth')->nullable(false)->change();
+            $table->string('spouse_occupation')->nullable(false)->change();
+            $table->string('spouse_contact_no')->nullable(false)->change();
+            $table->string('childrens')->nullable(false)->change();
+            $table->string('weight')->nullable(false)->change();
+            $table->string('height')->nullable(false)->change();
             $table->dropColumn('weight');
             $table->dropColumn('height');
         });
