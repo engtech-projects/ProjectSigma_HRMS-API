30c1a8db432911186175f503569412190293568d
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-02-23 13:31:59 +0800

    Update ManpowerRequestController.php

--- app/Http/Controllers/ManpowerRequestController.php
+++ app/Http/Controllers/ManpowerRequestController.php
@@ -41,7 +41,10 @@ class ManpowerRequestController extends Controller
      */
     public function get_hiring()
     {
-        $main = ManpowerRequest::where("request_status",'=','Approved')->get();
+        $main = ManpowerRequest::with('job_applicants')->where("request_status",'=','Approved')->get();
+        // $main = ManpowerRequest::with('job_applicants', function($query){
+        //     return $query->where("request_status",'=','Approved');
+        // })->get();
         $data = json_decode('{}');
         $data->message = "Successfully fetch.";
         $data->success = true;
