767de78a0fd2e23b496b54e8e352ed9b45b93808
Giejie <geiizanityy@gmail.com>
2024-03-06 08:28:15 +0800

    Update exception message

--- app/Exceptions/TransactionFailedException.php
+++ app/Exceptions/TransactionFailedException.php
@@ -7,10 +7,9 @@ use Exception;
 class TransactionFailedException extends Exception
 {
     protected $code;
-    public function __construct(?string $prefix, $message = "Transaction Failed.", $code = 400, Exception $previous = null)
+    public function __construct($message = "Something went wrong, Transaction failed.", $code = 400, Exception $previous = null)
     {
         $this->code = $code;
-        $message = $prefix . " " . $message;
         parent::__construct($message, $code, $previous);
     }
     public function getStatusCode(): int
