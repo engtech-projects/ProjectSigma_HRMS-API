ec835c37146d6fd0920d449c11bb0508f5c2f145
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-04-02 15:18:04 +0800

    hmorequest fix on json_decode

--- app/Http/Requests/StoreHMORequest.php
+++ app/Http/Requests/StoreHMORequest.php
@@ -17,9 +17,11 @@ class StoreHMORequest extends FormRequest
 
     protected function prepareForValidation()
     {
-        $this->merge([
-            "hmo_members" => json_decode($this->hmo_members, true)
-        ]);
+        if (gettype($this->hmo_members) == "string") {
+            $this->merge([
+                "hmo_members" => json_decode($this->hmo_members, true)
+            ]);
+        }
     }
 
     /**
