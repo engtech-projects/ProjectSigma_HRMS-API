45790a9ef6e5ffcfe68a25f4c51b754c529f3a16
Genesis John Abobo <geiizanityy@gmail.com>
2024-04-04 14:01:00 +0800

    Update RouteServiceProvider.php

--- app/Providers/RouteServiceProvider.php
+++ app/Providers/RouteServiceProvider.php
@@ -42,8 +42,8 @@ class RouteServiceProvider extends ServiceProvider
         Route::bind('projectMonitoringId', function ($value) {
             try {
                 $project = Project::where('project_monitoring_id', $value)->firstOrfail();
-            } catch (\Exception $e) {
-                throw new NotFoundHttpException("Project not found.");
+            } catch (\Throwable $e) {
+                throw new NotFoundHttpException("Project not found.", $e, 404);
             }
             return $project;
         });
