f02056a6034d6f1eb37b9a0d6355e5aeb6995878
janlofredy <janlofredy@gmail.com>
2024-04-17 16:46:34 +0000

    fix typo

--- app/Http/Traits/UploadImageTrait.php
+++ app/Http/Traits/UploadImageTrait.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Traits;
 
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Log;
 use Illuminate\Support\Facades\Storage;
 
@@ -23,7 +24,7 @@ trait UploadImageTrait
             $hashmake = Hash::make('secret');
             $hashname = hash('sha256', $hashmake);
             $randName = mt_rand().time();
-            return Storage::put('images/' . $path . '/' . $hashName . '/' . $randName . $type, $file);
+            return Storage::put('images/' . $path . '/' . $hashname . '/' . $randName . $type, $file);
         }
     }
 }
