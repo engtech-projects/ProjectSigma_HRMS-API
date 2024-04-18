fc10f73481039e255ea5a557822052357e972c32
Giejie <geiizanityy@gmail.com>
2024-04-18 10:21:42 +0800

    Update ImageController.php

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -16,14 +16,28 @@ class ImageController extends Controller
     public function uploadProfileImage(Request $request, $id)
     {
         $employee = Employee::findOrFail($id);
+        $parentable_type = get_class($employee);
         $profileImage = $this->uploadImage($request, 'profile_picture', $employee);
         if ($profileImage) {
-            Image::create([
-                'url' => $profileImage,
-                'image_type' => "profile_image",
-                'parentable_id' => $employee->id,
-                'parentable_type' => Employee::class
-            ]);
+            $profilePhoto = Image::where('parentable_id', $employee->id)
+                ->where('image_type', 'profile_image')
+                ->where('parentable_type', $parentable_type)
+                ->first();
+            if ($profilePhoto) {
+                $profilePhoto->update([
+                    'url' => $profileImage,
+                    'image_type' => "profile_image",
+                    'parentable_id' => $employee->id,
+                    'parentable_type' => Employee::class
+                ]);
+            } else {
+                Image::create([
+                    'url' => $profileImage,
+                    'image_type' => "profile_image",
+                    'parentable_id' => $employee->id,
+                    'parentable_type' => Employee::class
+                ]);
+            }
         }
         return new JsonResponse([
             'success' => true,
@@ -36,12 +50,12 @@ class ImageController extends Controller
         $parentable_type = get_class($employee);
         $file = $this->uploadImage($request, 'digital_signature', $employee);
         if ($file) {
-            $profilePhoto = Image::where('parentable_id', $employee)
+            $signature = Image::where('parentable_id', $employee->id)
                 ->where('image_type', 'signature')
                 ->where('parentable_type', $parentable_type)
                 ->first();
-            if ($profilePhoto) {
-                $profilePhoto->update([
+            if ($signature) {
+                $signature->update([
                     'url' => $file,
                     'image_type' => "signature",
                     'parentable_id' => $employee->id,
