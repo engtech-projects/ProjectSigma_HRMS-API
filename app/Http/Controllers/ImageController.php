d1251a6c7c0a735ccbec5d36396d221057759fbd
Giejie <geiizanityy@gmail.com>
2024-04-18 09:43:53 +0800

    Base64 file fix

--- app/Http/Controllers/ImageController.php
+++ app/Http/Controllers/ImageController.php
@@ -15,16 +15,13 @@ class ImageController extends Controller
 
     public function uploadProfileImage(Request $request, $id)
     {
-        $employee_id = Employee::findOrFail($id);
-        Log::info("Request In uploadProfileImage");
-        Log::info($request);
-        $profileImage = $this->uploadImage($request, 'profile_picture',$employee);
-        Log::info($profileImage);
+        $employee = Employee::findOrFail($id);
+        $profileImage = $this->uploadImage($request, 'profile_picture', $employee->id);
         if ($profileImage) {
             Image::create([
-                'url' => $url,
+                'url' => $profileImage,
                 'image_type' => "profile_image",
-                'parentable_id' => $employee_id->id,
+                'parentable_id' => $employee->id,
                 'parentable_type' => Employee::class
             ]);
         }
@@ -38,7 +35,6 @@ class ImageController extends Controller
         $employee = Employee::findOrFail($id);
         $parentable_type = get_class($employee);
         $file = $this->uploadImage($request, 'digital_signature', $employee);
-        $url = 'images/digital_signature/' . $file->hashName() . '/' . $file->getClientOriginalName();
         if ($file) {
             $profilePhoto = Image::where('parentable_id', $employee->id)
                 ->where('image_type', 'signature')
@@ -46,14 +42,14 @@ class ImageController extends Controller
                 ->first();
             if ($profilePhoto) {
                 $profilePhoto->update([
-                    'url' => $url,
+                    'url' => $file,
                     'image_type' => "signature",
                     'parentable_id' => $employee->id,
                     'parentable_type' => Employee::class
                 ]);
             } else {
                 Image::create([
-                    'url' => $url,
+                    'url' => $file,
                     'image_type' => "signature",
                     'parentable_id' => $employee->id,
                     'parentable_type' => Employee::class
--- app/Http/Traits/UploadImageTrait.php
+++ app/Http/Traits/UploadImageTrait.php
@@ -3,13 +3,14 @@
 namespace App\Http\Traits;
 
 use App\Models\Image;
+use Illuminate\Support\Str;
+use Illuminate\Support\Facades\Hash;
 use Illuminate\Support\Facades\Storage;
 
 trait UploadImageTrait
 {
     public function uploadImage($request, $path, $employee)
     {
-
         if ($request->hasFile('image_file')) {
             $file = $request->file('image_file');
             $hashName = $file->hashName();
@@ -25,14 +26,26 @@ trait UploadImageTrait
             $file->storePubliclyAs('images/' . $path . '/' . $hashName, $filename, 'public');
             return $file;
         } else if (gettype($request->input('image_file')) == "string") {
-            list($mime, $data)   = explode(';', $request->input('image_file'));
+            $img_64 = $request->input('image_file');
+            $extension = explode('/', explode(':', substr($img_64, 0, strpos($img_64, ';')))[1])[1];
+            $replace = substr($img_64, 0, strpos($img_64, ',') + 1);
+            $image = str_replace($replace, '', $img_64);
+            $image = str_replace(' ', '+', $image);
+
+            $imageName = Str::random(10) . '.' . $extension;
+            $url = 'images/' . $path . '/' . $imageName . '/' . $imageName;
+            $randName = mt_rand() . time();
+            $url = 'images/' . $path . '/' . $randName . '/' . $imageName;
+            Storage::disk('public')->put($url, base64_decode($image));
+            return Storage::url($url);
+            /* list($mime, $data)   = explode(';', $request->input('image_file'));
             list(, $data)       = explode(',', $data);
             list(, $type)       = explode('/', $mime);
             $file = base64_decode($data);
             $hashmake = Hash::make('secret');
             $hashname = hash('sha256', $hashmake);
-            $randName = mt_rand().time();
-            return Storage::put('images/' . $path . '/' . $hashname . '/' . $randName . $type, $file);
+            $randName = mt_rand() . time();
+            Storage::put('images/' . $path . '/' . $hashname . '/' . $randName . $type, $file); */
         }
     }
 }
