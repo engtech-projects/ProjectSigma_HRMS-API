26b7b8c51607ce6932c30aee3e7894b46b669270
Genesis John Abobo <geiizanityy@gmail.com>
2024-04-22 15:42:35 +0800

    Update 2024_04_22_102333_update_employee_pan_request_table.php

--- database/migrations/2024_04_22_102333_update_employee_pan_request_table.php
+++ database/migrations/2024_04_22_102333_update_employee_pan_request_table.php
@@ -1,8 +1,9 @@
 <?php
 
-use Illuminate\Database\Migrations\Migration;
-use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\DB;
 use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
 
 return new class extends Migration
 {
@@ -13,12 +14,13 @@ return new class extends Migration
     {
         Schema::table('employee_pan_requests', function (Blueprint $table) {
             $tableName = 'employee_pan_requests';
-            if (Schema::hasColumn($tableName, 'new_salary_grades')) {
-                $table->dropForeign(['new_salary_grades']);
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
+            if (Schema::hasColumn('employee_pan_requests', 'new_salary_grades')) {
+                $table->dropForeign('new_salary_grades');
                 $table->dropColumn('new_salary_grades');
             }
             if (Schema::hasColumn($tableName, 'new_section_id')) {
-                $table->dropForeign(['new_section_id']);
+                $table->dropForeign('new_section_id');
                 $table->dropColumn('new_section_id');
             }
             if (Schema::hasColumn($tableName, 'new_position')) {
@@ -30,6 +32,7 @@ return new class extends Migration
             if (Schema::hasColumn($tableName, 'new_employment_status')) {
                 $table->dropColumn('new_employment_status');
             }
+            DB::statement('SET FOREIGN_KEY_CHECKS=0;');
         });
     }
 
@@ -38,7 +41,7 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('employee_personnel_action_notice_requests', function (Blueprint $table) {
+        Schema::table('employee_pan_requests', function (Blueprint $table) {
         });
     }
 };
