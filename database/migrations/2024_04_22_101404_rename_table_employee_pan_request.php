e11b3e38b1ffdc7b198ecf2a44c58e7a7f206f4e
Genesis John Abobo <geiizanityy@gmail.com>
2024-04-22 13:09:32 +0800

    Update Employee Pan Requests table

---
+++ database/migrations/2024_04_22_101404_rename_table_employee_pan_request.php
@@ -0,0 +1,30 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employee_personnel_action_notice_requests', function (Blueprint $table) {
+            if (Schema::hasTable('employee_personnel_action_notice_requests')) {
+                Schema::rename('employee_personnel_action_notice_requests', 'employee_pan_requests');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employee_personnel_action_notice_requests', function (Blueprint $table) {
+            //
+        });
+    }
+};
---
+++ database/migrations/2024_04_22_102333_update_employee_pan_request_table.php
@@ -0,0 +1,44 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::table('employee_pan_requests', function (Blueprint $table) {
+            $tableName = 'employee_pan_requests';
+            if (Schema::hasColumn($tableName, 'new_salary_grades')) {
+                $table->dropForeign(['new_salary_grades']);
+                $table->dropIndex('new_salary_grades');
+            }
+            if (Schema::hasColumn($tableName, 'new_section_id')) {
+                $table->dropForeign(['new_section_id']);
+                $table->dropColumn('new_section_id');
+            }
+            if (Schema::hasColumn($tableName, 'new_position')) {
+                $table->dropColumn('new_position');
+            }
+            if (Schema::hasColumn($tableName, 'new_location')) {
+                $table->dropColumn('new_location');
+            }
+            if (Schema::hasColumn($tableName, 'new_employment_status')) {
+                $table->dropColumn('new_employment_status');
+            }
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::table('employee_personnel_action_notice_requests', function (Blueprint $table) {
+        });
+    }
+};
