f428deb3b65d78c3ddf22f918f88076aee55581f
Genesis John Abobo <geiizanityy@gmail.com>
2024-05-03 11:32:58 +0800

    Employee DTR update

--- app/Http/Controllers/Actions/Attendance/EmployeeDtrController.php
+++ app/Http/Controllers/Actions/Attendance/EmployeeDtrController.php
@@ -2,12 +2,14 @@
 
 namespace App\Http\Controllers\Actions\Attendance;
 
+use App\Helpers;
 use App\Http\Controllers\Controller;
-use App\Http\Requests\FilterDtrRequest;
+use App\Http\Requests\GenerateDtrRequest;
 use App\Http\Resources\AttendanceLogResource;
 use App\Http\Resources\EmployeeLeaveResource;
 use App\Http\Resources\InternalWorkExpResource;
 use App\Http\Resources\OvertimeResource;
+use App\Http\Services\EmployeeService;
 use App\Models\Employee;
 use Illuminate\Http\JsonResponse;
 
@@ -15,101 +17,31 @@ use Illuminate\Http\JsonResponse;
 class EmployeeDtrController extends Controller
 {
 
-    protected $employee;
-    public function __construct(Employee $employee)
-    {
-        $this->employee = $employee;
-    }
     /**
      * Handle the incoming request.
      */
-    public function __invoke(FilterDtrRequest $request)
+
+    protected $employeeService;
+    public function __invoke(GenerateDtrRequest $request, EmployeeService $employeeService)
     {
-        $data = $this->getSchedule($request->validated());
+        $this->employeeService = $employeeService;
+        $filters = $request->validated();
+        $periodDates = Helpers::dateRange([
+            'period_start' => $filters["cutoff_start"], 'period_end' => $filters["cutoff_end"]
+        ]);
+        $employees = Employee::whereIn('id', $filters['employee_ids'])->get();
+        $result = collect($periodDates)->groupBy(function ($date) {
+            return $date["date"];
+        })->map(function ($date) use ($employees) {
+            $date = $date[0]["date"];
+            foreach ($employees as $employee) {
+                return $this->employeeService->employeeDTR($employee, $date);
+            }
+        });
         return new JsonResponse([
             'success' => true,
             'message' => 'Successfully fetched.',
-            'data' => $data
-
+            'data' => $result
         ]);
-    }
-
-    private function getSchedule(array $filter = [])
-    {
-        $employee = $this->employee->with(['employee_internal', 'employee_leave', 'employee_overtime', 'employee_has_projects', 'employee_schedule' => function ($query) use ($filter) {
-            $query->where(function ($query) use ($filter) {
-                $query->where('startRecur', '>=', $filter['period_start']);
-            });
-        }])->where('id', $filter['employee_id'])->first();
-
-        $schedule = $this->mapResultSchedule($employee);
-        return $schedule;
-    }
-
-    private function mapResultSchedule($employee)
-    {
-        $employee->employee_schedule = collect($employee["employee_schedule"])->groupBy(function ($schedule) use ($employee) {
-            $dateSchedule = $schedule->startRecur->format('F j, Y');
-            return $dateSchedule;
-        })->toArray();
-
-        $employee->employee_leave = $employee->employee_leave->where('with_pay', true);
-
-        $employee->employee_schedule = collect($employee->employee_schedule)->map(function ($schedule) {
-            return $schedule;
-        });
-
-
-        $employee->employee_schedule = $this->filterSchedule($employee->employee_schedule);
-        return [
-            "id" => $employee->id,
-            "employee_name" => $employee->fullname_first,
-            "employee_internal_exp" => InternalWorkExpResource::collection($employee->employee_internal),
-            "employee_schedule" => collect($employee->employee_schedule)->map(function ($schedule) use ($employee) {
-                return collect($schedule)->map(function ($value) use ($employee) {
-                    return [
-                        "schedule" => $value,
-                        "logs" => $employee->attendance_log, //AttendanceLogResource::collection($employee->attendance_log),
-                        "leaves" => $employee->employee_leave,  //EmployeeLeaveResource::collection($employee->employee_leave),
-                        "overtime" => $employee->employee_overtime, //OvertimeResource::collection($employee->employee_overtime),
-                    ];
-                });
-            }),
-            "employee_projects" => $employee->employee_has_projects,
-        ];
-
-        return $employee->employee_schedule;
-    }
-
-    private function filterSchedule($schedule)
-    {
-        $employeeSchedule = collect($schedule)->map(function ($schedule) {
-            $hasIrregSchedule = collect($schedule)->contains('scheduleType', 'Irregular');
-            $employeeGroupFiltered = collect($schedule)->filter(function ($value) use ($hasIrregSchedule) {
-                if ($hasIrregSchedule) {
-                    return $value["groupType"] == 'employee' && $value["scheduleType"] == 'Irregular';
-                }
-                return  $value["groupType"] == 'employee' &&  $value["scheduleType"] == 'Regular';
-            })->values();
-
-            if (empty($employeeGroupFiltered)) {
-                $employeeGroupFiltered = collect($schedule)->filter(function ($value) {
-                    return $value["groupType"] != 'employee';
-                })->all();
-            }
-
-            return $employeeGroupFiltered->map(function ($schedule) {
-                return [
-                    "groupType" => $schedule["groupType"],
-                    "scheduleType" => $schedule["scheduleType"],
-                    "daysOfWeek" => $schedule["daysOfWeek"],
-                    "startTime" => $schedule["startTime"],
-                    "endTime" => $schedule["endTime"],
-                    "startRecur" => $schedule["startRecur"],
-                    "endRecur" => $schedule["endRecur"],
-                ];
-            });
-        })->all();
-        return array_filter($employeeSchedule);
     }
 }
--- app/Http/Controllers/Actions/GeneratePayrollController.php
+++ app/Http/Controllers/Actions/GeneratePayrollController.php
@@ -14,20 +14,14 @@ use Illuminate\Http\JsonResponse;
 
 class GeneratePayrollController extends Controller
 {
-    use Attendance, DailyTimeRecord;
-
-    protected $employeeService;
-    public function __construct(EmployeeService $employeeService)
-    {
-        $this->employeeService = $employeeService;
-    }
-
     /**
      * Handle the incoming request.
      */
+    protected $employeeService;
     protected $attendance;
-    public function __invoke(GeneratePayrollRequest $request)
+    public function __invoke(GeneratePayrollRequest $request, EmployeeService $employeeService)
     {
+        $this->employeeService = $employeeService;
         $filters = $request->validated();
         $periodDates = Helpers::dateRange([
             'period_start' => $filters["cutoff_start"], 'period_end' => $filters["cutoff_end"]
@@ -41,48 +35,10 @@ class GeneratePayrollController extends Controller
                 return $this->employeeService->employeeDTR($employee, $date);
             }
         });
-        return $result;
-
-        foreach ($periodDates as $dateKey => $date) {
-            foreach ($employees as $empKey => $employee) {
-                $result[] = [
-                    $dateKey => $employee
-                ];
-            }
-        }
-        return $result;
-        $employees = Employee::with([
-            'employee_leave' => function ($query) use ($filters) {
-                return $query->payrollLeave($filters)->approved();
-            },
-            'employee_has_overtime' => function ($query) use ($filters) {
-                return $query->payrollOvertime($filters)->approved();
-            },
-            'attendance_log' => function ($query) use ($filters) {
-                return $query->payrollAttendanceLog($filters);
-            },
-            'employee_schedule' => function ($query) use ($filters) {
-                return $query->payrollSchedule($filters);
-            },
-        ])->whereIn('id', $filters['employee_ids'])->get();
-
-        $result = [];
-        foreach ($employees as $employee) {
-            $workingInterval = $this->calculateAttendanceLog($employee->attendance_log);
-            $workingLateInterval = $this->calculateAttedanceLate($employee->attendance_log, $employee->employee_schedule);
-            $schedules = collect($employee->employee_schedule)->groupBy('startRecur');
-            return $result[] = [
-                "total_of_hours" => $workingInterval->totalHours,
-                "total_of_days" => $workingInterval->totalDays,
-                "total_of_minutes" => $workingInterval->totalMinutes,
-                "total_of_lates" => $workingLateInterval
-            ];
-        }
-
         return new JsonResponse([
             'success' => true,
             'message' => 'Successfully fetched.',
-            'data' => $employees
+            'data' => $result
         ]);
     }
 }
