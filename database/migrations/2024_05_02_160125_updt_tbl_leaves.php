7f18ec21bce3efb1541a8bd0f7a7637153e22f42
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2024-05-06 03:35:09 +0000

    fix migration and comma for address

--- database/migrations/2024_05_02_160125_updt_tbl_leaves.php
+++ database/migrations/2024_05_02_160125_updt_tbl_leaves.php
@@ -4,15 +4,14 @@ use Illuminate\Database\Migrations\Migration;
 use Illuminate\Database\Schema\Blueprint;
 use Illuminate\Support\Facades\Schema;
 
-return new class extends Migration
-{
+return new class () extends Migration {
     /**
      * Run the migrations.
      */
     public function up(): void
     {
         Schema::table('leaves', function (Blueprint $table) {
-            $table->renameColumn('employment_type', 'employment_status');
+            $table->renameColumn('`employment_type`', '`employment_status`');
         });
     }
 
