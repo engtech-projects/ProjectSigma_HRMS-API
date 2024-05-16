cfdc6c405f2fe8d5bf9260f58611c9687f5231ad
Genesis John Abobo <geiizanityy@gmail.com>
2024-05-16 14:18:30 +0800

    Update PaginateResourceCollection.php

--- app/Utils/PaginateResourceCollection.php
+++ app/Utils/PaginateResourceCollection.php
@@ -13,11 +13,9 @@ class PaginateResourceCollection
     {
 
         $pageNumber = Paginator::resolveCurrentPage('page');
-
         $totalPageNumber = $results->count();
         $offset = ($pageNumber * $showPerPage) - $showPerPage;
         $items = collect(array_slice($results->toArray(), $offset, $showPerPage));
-        /*         return new LengthAwarePaginator($items, $totalPageNumber, $showPerPage); */
         return self::paginator($items, $totalPageNumber, $showPerPage, $pageNumber, [
             'path' => Paginator::resolveCurrentPath(),
             'pageName' => 'page',
