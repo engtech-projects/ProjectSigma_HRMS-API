7de9706439eb40afaa739e9d439fd2b069ea3114
Rustom Pedales <pedales.rustom@gmail.com>
2024-05-27 14:13:23 +0800

    Update EmployeeBulkUploadController.php

--- app/Http/Controllers/EmployeeBulkUploadController.php
+++ app/Http/Controllers/EmployeeBulkUploadController.php
@@ -334,7 +334,7 @@ class EmployeeBulkUploadController extends Controller
                         $departmentId = $this->getDepartmentId(trim($data['section_project_code']));
                     }
                     $postionId = $this->getPositionId($data['position'], $departmentId);
-                    $getSalaryStep = $this->getSalaryStep($this->getSalaryGradeLevelId($data['salary_grade_level']));
+                    $getSalaryStep = $this->getSalaryStep($this->getSalaryGradeLevelId($data['salary_grade_level']), $data['salary_grade_step']);
                     $internalRecord = [
                         'position_id' => $postionId,
                         'employment_status' => $data['employment_status'],
@@ -557,9 +557,9 @@ class EmployeeBulkUploadController extends Controller
         $data = SalaryGradeLevel::where('salary_grade_level', $salaryGradeLevel)->first();
         return $data ? $data->id : null;
     }
-    public function getSalaryStep($salaryGradeLevelId)
+    public function getSalaryStep($salaryGradeLevelId, $stepName)
     {
-        $data = SalaryGradeStep::where('salary_grade_level_id', $salaryGradeLevelId)->first();
+        $data = SalaryGradeStep::where(['salary_grade_level_id' => $salaryGradeLevelId, 'step_name' => $stepName, ])->first();
         return $data;
     }
 }
