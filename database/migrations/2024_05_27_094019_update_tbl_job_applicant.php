73dfa2019ec5892414c04d7a02993ff47a9c8a88
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-05-27 11:05:11 +0800

    update varchar col on tbl job applicants

--- database/migrations/2024_05_27_094019_update_tbl_job_applicant.php
+++ database/migrations/2024_05_27_094019_update_tbl_job_applicant.php
@@ -14,7 +14,7 @@ return new class extends Migration
         Schema::table('job_applicants', function (Blueprint $table) {
             $table->string('contact_info')->nullable()->change();
             $table->string('telephone_spouse')->nullable()->change();
-            $table->string('telephone_icoe')->nullable()->change();
+            $table->string('telephone_icoe')->change();
         });
     }
 
@@ -26,7 +26,7 @@ return new class extends Migration
         Schema::table('job_applicants', function (Blueprint $table) {
             $table->bigInteger('contact_info')->nullable()->change();
             $table->bigInteger('telephone_spouse')->nullable()->change();
-            $table->bigInteger('telephone_icoe')->nullable()->change();
+            $table->bigInteger('telephone_icoe')->change();
         });
     }
 };
