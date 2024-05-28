e90f16f9f5f6a2571669290370555f25b89cc9b8
Methuselah Daniel Doysabas <124086485+DanielDoysabas@users.noreply.github.com>
2024-05-28 21:07:41 +0800

    Revert "add approvals on generate allowance"
    This reverts commit 0807b8751f75ae2950e2e6ead62c7cd4356f0f1b.

--- app/Models/Allowance.php
+++ app/Models/Allowance.php
@@ -3,7 +3,6 @@
 namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
-use App\Traits\HasApproval;
 use Illuminate\Database\Eloquent\Model;
 use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Notifications\Notifiable;
@@ -16,24 +15,17 @@ class Allowance extends Model
     use HasFactory;
     use Notifiable;
     use SoftDeletes;
-    use HasApproval;
 
     protected $table = 'allowances';
-
-    protected $casts = [
-        'approvals' => 'array',
-    ];
 
     protected $fillable = [
         'id',
         'position_id',
         'amount',
-        'approvals',
     ];
 
     public function position(): BelongsTo
     {
         return $this->belongsTo(Position::class);
     }
-
 }
