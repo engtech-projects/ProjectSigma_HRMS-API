691e1458dca6f24326e00f2b50ee9fe431706415
Rustom Pedales <pedales.rustom@gmail.com>
2024-06-24 14:08:07 +0800

    [Update]: Payroll mail message indent

--- app/Notifications/PayrollRequestForApproval.php
+++ app/Notifications/PayrollRequestForApproval.php
@@ -7,7 +7,7 @@ use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Notifications\Messages\MailMessage;
 use Illuminate\Notifications\Notification;
 
-class PayrollRequestApproval extends Notification
+class PayrollRequestForApproval extends Notification
 {
     use Queueable;
 
@@ -35,10 +35,10 @@ class PayrollRequestApproval extends Notification
     public function toMail(object $notifiable): MailMessage
     {
         return (new MailMessage)
-                    ->line('The introduction to the notification.')
-                    ->action('Notification Action', url('/'))
-                    ->line('Thank you for using our application!');
-    }
+            ->line('The introduction to the notification.')
+            ->action('Notification Action', url('/'))
+            ->line('Thank you for using our application!');
+}
 
     /**
      * Get the array representation of the notification.
