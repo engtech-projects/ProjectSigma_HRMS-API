10dd31e9203c768989b7d040c42f2d50464e97a3
Rustom Pedales <pedales.rustom@gmail.com>
2024-07-05 10:29:26 +0800

    [BugFix]: Count department statistics

--- app/Http/Controllers/Actions/Employee/CountEmployeeDepartmentController.php
+++ app/Http/Controllers/Actions/Employee/CountEmployeeDepartmentController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Actions\Employee;
 
+use App\Enums\InternalWorkExpStatus;
 use Illuminate\Http\Request;
 use Illuminate\Http\JsonResponse;
 use App\Http\Controllers\Controller;
@@ -13,22 +14,23 @@ class CountEmployeeDepartmentController extends Controller
 
     public function __invoke()
     {
-        $employeeByDepartment = Department::with(['internal_work_exp'])->get();
-        $employeeNoDepartment = InternalWorkExperience::whereNull('department_id')->count();
-        $employeeByDepartment = collect($employeeByDepartment)->groupBy(function ($department) {
-            $deptName = $department->department_name;
-            return $deptName;
-        })->map(function ($val) {
-            return $val[0]->internal_work_exp->count();
+        $employeeByDepartment = Department::with(['internal_work_exp'=> function($query) {
+            return $query->where('status',InternalWorkExpStatus::CURRENT)->get();
+        }])->get();
+        $employeeNoDepartment = InternalWorkExperience::whereNull('department_id')->where('status',InternalWorkExpStatus::CURRENT)->count();
+        $employeeByDepartment = collect($employeeByDepartment)->flatMap(function ($val) {
+            return [
+                $val->department_name => $val->internal_work_exp->count()
+            ];
         });
-
-        $employeeByDepartment->put('No Department', $employeeNoDepartment);
+        // $employeeByDepartment = $employeeByDepartment->flatten();
+        $employeeByDepartment->put('NO DEPARTMENT', $employeeNoDepartment);
 
 
         return new JsonResponse([
             'success' => true,
             'message' => 'Successfully fetched.',
-            'data' => $employeeByDepartment
+            'data' => $employeeByDepartment,
         ]);
     }
 }
