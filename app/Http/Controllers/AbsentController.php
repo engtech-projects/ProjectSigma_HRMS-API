4474ce33f569c9c18a80d10831ad6dda2e3736f8
Rustom Pedales <pedales.rustom@gmail.com>
2024-08-09 09:48:41 +0800

    [BugFix] Absences

--- app/Http/Controllers/AbsentController.php
+++ app/Http/Controllers/AbsentController.php
@@ -6,6 +6,7 @@ use App\Enums\AttendanceLogType;
 use App\Models\AttendanceLog;
 use App\Models\Employee;
 use App\Models\Schedule;
+use Carbon\CarbonPeriod;
 use Illuminate\Http\JsonResponse;
 use Illuminate\Support\Carbon;
 
@@ -13,23 +14,38 @@ class AbsentController extends Controller
 {
     public function getAbsenceThisMonth(Schedule $req, AttendanceLog $log)
     {
-        $attendance = AttendanceLog::whereBetween('date', [
-            Carbon::now()->startOfMonth(),
-            Carbon::now()->lastOfMonth()
-        ])->where('log_type', AttendanceLogType::TIME_IN->value)->with(['department.schedule', 'project.project_schedule'])->get();
-        return array_values($attendance->where(function($attendance) {
-            if ($attendance->department_id != null) {
-                return sizeof($attendance->department->schedule) > 0;
-            }
-        })->countBy("employee_id")->map(function($val, $key) {
-            $emp = Employee::find($key);
-            return[
-                'employee_id' => $key,
-                'fullname_first' => $emp->fullname_first,
-                'fullname_last' => $emp->fullname_last,
-                'profile_photo' => $emp->profile_photo(),
-                'absent' => $val
+        $startOfMonth = Carbon::now()->startOfMonth();
+        $endOfMonth = Carbon::now()->endOfMonth();
+        $period = CarbonPeriod::create($startOfMonth, $endOfMonth);
+        // Filter the period to include only Sundays
+        $sundayDays = $period->filter(function (Carbon $date) {
+            return $date->isSunday();
+        })->toArray();
+
+        $sundaysThisMonth = sizeof($sundayDays);
+        $daysThisMonth = $endOfMonth->format('d');
+        $workDaysCount = $daysThisMonth - $sundaysThisMonth;
+
+        $attendance = Employee::with(['attendance_log' => function($query) use ($sundayDays) {
+            $query->whereBetween('date', [
+                Carbon::now()->startOfMonth(),
+                Carbon::now()->endOfMonth()
+            ])
+            ->whereNotIn('date', $sundayDays)
+            ->where('log_type', AttendanceLogType::TIME_IN->value);
+        }, 'attendance_log.department.schedule', 'attendance_log.project.project_schedule'])
+        ->get();
+
+        return $attendance->map(function($employee) use ($workDaysCount) {
+            $attendedDays = $employee->attendance_log->count();
+            return [
+                'fullname_first' => $employee->fullname_first,
+                'fullname_last' => $employee->fullname_last,
+                'profile_photo' => $employee->profile_photo(),
+                'absent' => $workDaysCount - $attendedDays,
+                'workDaysCount' => $workDaysCount,
+                'attendDays' => $attendedDays,
             ];
-        })->toArray());
+        });
     }
 }
