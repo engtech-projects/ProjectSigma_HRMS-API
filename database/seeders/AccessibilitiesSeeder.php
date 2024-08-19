b17f0aa5d83d85ce4537f05b34a95cd3423fc531
Rustom Pedales <pedales.rustom@gmail.com>
2024-08-19 15:06:32 +0800

    [Update]: remove my request leave

--- database/seeders/AccessibilitiesSeeder.php
+++ database/seeders/AccessibilitiesSeeder.php
@@ -722,8 +722,8 @@ class AccessibilitiesSeeder extends Seeder
                 ],
                 [
                     'id' => 102,
-                    'accessibilities_name' => AccessibilityHrms::HRMS_LNOTNTO_LEAVE_MYREQUEST->value,
-                    'deleted_at' => null,
+                    'accessibilities_name' => null,
+                    'deleted_at' => Carbon::now(),
                     'created_at' => Carbon::now(),
                     'updated_at' => Carbon::now(),
                 ],
