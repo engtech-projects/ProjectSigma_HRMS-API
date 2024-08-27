dab6cdd83cc2693f8eca2d38c5a344c592846d78
Jose Janlofre Dy <janlofredy@gmail.com>
2024-08-27 15:09:23 +0800

    log viewer add nginx errors for evenpar deployment

--- config/log-viewer.php
+++ config/log-viewer.php
@@ -142,6 +142,7 @@ return [
         // You can include paths to other log types as well, such as apache, nginx, and more.
         '/var/log/httpd/*',
         '/var/log/nginx/*',
+        '../../../../logs/*',
 
         // MacOS Apple Silicon logs
         '/opt/homebrew/var/log/nginx/*',
