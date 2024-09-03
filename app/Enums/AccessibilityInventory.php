dc1eae58f8f7024f8a6da5b73b4cc86c8e76bf44
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-09-03 11:12:29 +0800

    Update AccessibilityInventory.php

--- app/Enums/AccessibilityInventory.php
+++ app/Enums/AccessibilityInventory.php
@@ -10,9 +10,9 @@ enum AccessibilityInventory: string
     case INVENTORY_SETUP_UNITOFMEASUREMENT = "inventory:setup_unit of measurements";
     case INVENTORY_ITEMPROFILE_NEW_PROFILE = "inventory:item profile_new profile";
     case INVENTORY_ITEMPROFILE_ITEMLIST = "inventory:item profile_list profile";
-    case INVENTORY_ITEMPROFILE_NEW_ALLREQUESTS = "inventory:item profile_new profile all request";
-    case INVENTORY_ITEMPROFILE_NEW_MYAPPROVALS = "inventory:item profile_new profile my approvals";
-    case INVENTORY_ITEMPROFILE_NEW_FORM = "inventory:item profile_new profile forms and my requests";
+    case INVENTORY_ITEMPROFILE_NEW_ALLREQUESTS = "inventory:item profile_new profile_all request";
+    case INVENTORY_ITEMPROFILE_NEW_MYAPPROVALS = "inventory:item profile_new profile_my approvals";
+    case INVENTORY_ITEMPROFILE_NEW_FORM = "inventory:item profile_new profile_forms and my requests";
 
     public static function toArray(): array
     {
