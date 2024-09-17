a3df589f08bad4cbc8bab31c7dfd0a8a9ddb7b6a
Jose Janlofre Dy <janlofredy@gmail.com>
2024-09-17 09:05:46 +0800

    Update 2024_09_17_080100_fix_payroll_records_grouptype_name.php

--- database/migrations/2024_09_17_080100_fix_payroll_records_grouptype_name.php
+++ database/migrations/2024_09_17_080100_fix_payroll_records_grouptype_name.php
@@ -11,8 +11,8 @@ return new class extends Migration
      */
     public function up(): void
     {
-        Schema::table('overtime', function (Blueprint $table) {
-            $table->rename('group_tye', 'charging_type');
+        Schema::table('payroll_records', function (Blueprint $table) {
+            $table->renameColumn('group_tye', 'charging_type');
         });
     }
 
@@ -21,8 +21,8 @@ return new class extends Migration
      */
     public function down(): void
     {
-        Schema::table('overtime', function (Blueprint $table) {
-            $table->rename('charging_type', 'group_tye');
+        Schema::table('payroll_records', function (Blueprint $table) {
+            $table->renameColumn('charging_type', 'group_tye');
         });
     }
 };
