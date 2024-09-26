f5e693685a7e554b31f3d3451e40fca8b7f63de4
Jose Janlofre Dy <janlofredy@gmail.com>
2024-09-26 14:15:35 +0800

    get Daily Applied Schedule

--- app/Http/Services/Attendance/AttendanceService.php
+++ app/Http/Services/Attendance/AttendanceService.php
@@ -4,12 +4,14 @@ namespace App\Http\Services\Attendance;
 
 use App\Enums\AttendanceLogType;
 use App\Enums\PayrollType;
+use App\Enums\WorkLocation;
 use App\Helpers;
 use App\Models\Employee;
 use App\Models\Events;
 use App\Models\PayrollRecord;
 use Carbon\Carbon;
 use Carbon\CarbonPeriod;
+use Illuminate\Support\Facades\Log;
 
 class AttendanceService
 {
@@ -25,6 +27,7 @@ class AttendanceService
         return $employee ->attendance_log->groupBy("date")->count();
     }
 
+    // Version 2 Of Generate DTR - Optimized, query all at beginning
     public static function generateDtr($employeeId, $dateFrom, $dateTo)
     {
         $employee = Employee::with([
@@ -70,12 +73,11 @@ class AttendanceService
             },
         ])->find($employeeId);
         $employeeDatas = [
+            "employee" => $employee,
             "employee_schedules_irregular" => $employee->employee_schedule_irregular,
             "employee_schedules_regular" => $employee->employee_schedule_regular,
-            "project_schedules_irregular" => $employee->employee_has_projects, // To Get Applicable Project to Process Per day
-            "project_schedules_regular" => $employee->employee_has_projects, // To Get Applicable Project to Process Per day
-            "department_schedules_irregular" => $employee->employee_internal, // To Get Applicable Employment to Process Per day
-            "department_schedules_regular" => $employee->employee_internal, // To Get Applicable Employment to Process Per day
+            // DEPARTMENT SCHEDULE TO BE TAKEN FROM employee->"employee_has_projects"
+            // DEPARTMENT SCHEDULE TO BE TAKEN FROM employee->"employee_internal"
             "overtimes" => $employee->employee_overtime,
             "attendanceLogs" => $employee->attendance_log,
             "travel_orders" => $employee->employee_travel_order,
@@ -92,12 +94,180 @@ class AttendanceService
             'period_start' => $dateFrom, 'period_end' => $dateTo
         ]);
         return collect($periodDates)->groupBy("date")->map(function ($val, $date) use ($employeeDatas) {
-            
+            $carbonDate = Carbon::parse($date);
+            // Get applied Schedule for date
+            $appliedDateSchedule = Self::getAppliedDateSchedule($employeeDatas, $carbonDate);
+            $appliedDateOvertime = Self::getAppliedDateOvertime($employeeDatas, $carbonDate);
+            $appliedDateAttendanceLogs = Self::getAppliedDateAttendanceLogs($employeeDatas, $carbonDate);
+            $appliedDateTravelOrders = Self::getAppliedDateTravelOrders($employeeDatas, $carbonDate);
+            $appliedDateLeaves = Self::getAppliedDateLeaves($employeeDatas, $carbonDate);
+            $appliedDateEvents = Self::getAppliedDateEvents($employeeDatas, $carbonDate);
+            $processedMetaData = Self::calculateDateAttendanceMetaData($employeeDatas);
             return [
                 "date" => $date,
-                "schedule" => $employeeDatas['employee_schedules_regular'],
-                "metadata" => "WOW",
+                "schedule" => $appliedDateSchedule,
+                "metadata" => $processedMetaData,
+                "events" => $appliedDateEvents,
             ];
         });
+    }
+    public static function getAppliedDateSchedule($employeeDatas, $date)
+    {
+
+        $schedule = $employeeDatas["employee_schedules_irregular"]->where(function ($data) use ($date) {
+            return $date->eq($data->startRecur);
+        });
+        if ($schedule && sizeof($schedule) > 0) {
+            return $schedule;
+        }
+        $schedule = $employeeDatas["employee_schedules_regular"]->where(function ($data) use ($date) {
+            return $date->gte($data->startRecur) &&
+            in_array((string)$date->dayOfWeek, $data->daysOfWeek ?? []) &&
+            (
+                is_null($data->endRecur) ||
+                $date->lt($data->endRecur)
+            );
+        });
+        if ($schedule && sizeof($schedule) > 0) {
+            return $schedule;
+        }
+        $currentInternalOnDate = $employeeDatas["employee"]->employee_internal->where(function ($data) use ($date) {
+            return $date->gte($data->date_from) &&
+            (
+                $date->lte($data->date_to) ||
+                is_null($data->date_to)
+            );
+        })->first();
+        if ($currentInternalOnDate->work_location == WorkLocation::OFFICE->value) {
+            $schedule = $currentInternalOnDate['department_schedules_irregular']->where(function ($data) use ($date) {
+                return $date->eq($data->startRecur);
+            });
+            if ($schedule && sizeof($schedule) > 0) {
+                return $schedule;
+            }
+            $schedule = $currentInternalOnDate['department_schedules_regular']->where(function ($data) use ($date) {
+                return $date->gte($data->startRecur) &&
+                in_array((string)$date->dayOfWeek, $data->daysOfWeek ?? []) &&
+                (
+                    is_null($data->endRecur) ||
+                    $date->lt($data->endRecur)
+                );
+            });
+            if ($schedule && sizeof($schedule) > 0) {
+                return $schedule;
+            }
+        }
+        if ($currentInternalOnDate->work_location == WorkLocation::PROJECT->value) {
+            $latestProject = $employeeDatas["employee"]->employee_has_projects->first();
+            $schedule = $latestProject->schedule_irregular->where(function ($data) use ($date) {
+                return $date->eq($data->startRecur);
+            });
+            if ($schedule && sizeof($schedule) > 0) {
+                return $schedule;
+            }
+            $schedule = $latestProject->schedule_regular->where(function ($data) use ($date) {
+                return $date->gte($data->startRecur) &&
+                in_array((string)$date->dayOfWeek, $data->daysOfWeek ?? []) &&
+                (
+                    is_null($data->endRecur) ||
+                    $date->lt($data->endRecur)
+                );
+            })->all();
+            if ($schedule && sizeof($schedule) > 0) {
+                return $schedule;
+            }
+        }
+        return collect([]);
+    }
+    public static function getAppliedDateOvertime($employeeDatas, $date)
+    {
+
+    }
+    public static function getAppliedDateAttendanceLogs($employeeDatas, $date)
+    {
+
+    }
+    public static function getAppliedDateTravelOrders($employeeDatas, $date)
+    {
+
+    }
+    public static function getAppliedDateLeaves($employeeDatas, $date)
+    {
+
+    }
+    public static function getAppliedDateEvents($employeeDatas, $date)
+    {
+        return $employeeDatas["events"]->where(function ($data) use ($date) {
+            return $date->gte($data->start_date) && $date->lte($data->end_date);
+        });
+    }
+    public static function calculateDateAttendanceMetaData($schedule)
+    {
+        // OUTPUT MUST BE
+        // GET hrsWorked, Overtime, Late, Undertime
+        // Regular
+        // Regular_holidays
+        // Rest
+        // Special_holidays
+        // Total
+        // GET hrsWorked, Overtime
+        // Charging
+        // Then Charges each  attendance type
+        // Summary
+        $metaResult = [
+            "charging" => [
+                // Charging Structure for reg_hrs and overtime
+                // [
+                //     "model" = "" // Department Model or Project Model
+                //     "id" = "" // Id for the Model
+                //     "hrs_worked" = "" // Hrs Worked
+                // ],
+                "regular" => [
+                    "reg_hrs" => [],
+                    "overtime" => [],
+                ],
+                "rest" => [
+                    "reg_hrs" => [],
+                    "overtime" => [],
+                ],
+                "regular_holidays" => [
+                    "reg_hrs" => [],
+                    "overtime" => [],
+                ],
+                "special_holidays" => [
+                    "reg_hrs" => [],
+                    "overtime" => [],
+                ],
+            ],
+            "regular" => [
+                "reg_hrs" => 0,
+                "overtime" => 0,
+                "late" => 0,
+                "undertime" => 0,
+            ],
+            "rest" => [
+                "reg_hrs" => 0,
+                "overtime" => 0,
+                "late" => 0,
+                "undertime" => 0,
+            ],
+            "regular_holidays" => [
+                "reg_hrs" => 0,
+                "overtime" => 0,
+                "late" => 0,
+                "undertime" => 0,
+            ],
+            "special_holidays" => [
+                "reg_hrs" => 0,
+                "overtime" => 0,
+                "late" => 0,
+                "undertime" => 0,
+            ],
+            "summary" => [
+                "Chargings" => "",
+                ""
+            ]
+        ];
+        return $metaResult;
     }
 }
--- app/Models/Schedule.php
+++ app/Models/Schedule.php
@@ -166,21 +166,21 @@ class Schedule extends Model
     {
         return $query->where(function ($query2) use ($date) {
             $query2->where('scheduleType', self::TYPE_REGULAR)
-                ->where(function ($query3) use ($date) {
-                    $query3->where(function ($query4) use ($date) {
-                        $carbondate = new Carbon($date);
-                        $query4->whereDate('startRecur', '<=', $date)
-                            ->whereNotNull('endRecur')
-                            ->whereDate('endRecur', '>', $date)
-                            ->whereJsonContains("daysOfWeek", (string)$carbondate->dayOfWeek);
-                    })
-                        ->orWhere(function ($query5) use ($date) {
-                            $carbondate = new Carbon($date);
-                            $query5->whereDate('startRecur', '<=', $date)
-                                ->whereNull('endRecur')
-                                ->whereJsonContains("daysOfWeek", (string)$carbondate->dayOfWeek);
-                        });
+            ->where(function ($query3) use ($date) {
+                $query3->where(function ($query4) use ($date) {
+                    $carbondate = new Carbon($date);
+                    $query4->whereDate('startRecur', '<=', $date)
+                        ->whereNotNull('endRecur')
+                        ->whereDate('endRecur', '>', $date)
+                        ->whereJsonContains("daysOfWeek", (string)$carbondate->dayOfWeek);
+                })
+                ->orWhere(function ($query5) use ($date) {
+                    $carbondate = new Carbon($date);
+                    $query5->whereDate('startRecur', '<=', $date)
+                        ->whereNull('endRecur')
+                        ->whereJsonContains("daysOfWeek", (string)$carbondate->dayOfWeek);
                 });
+            });
         })->orWhere(function ($query6) use ($date) {
             $query6->where('scheduleType', self::TYPE_IRREGULAR)
                 ->whereDate('startRecur', '=', $date);
@@ -189,23 +189,27 @@ class Schedule extends Model
 
     public function scopeBetweenDates(Builder $query, $dateFrom, $dateTo)
     {
-        return $query->where(function($query) use ($dateFrom, $dateTo) {
-            $query->where(function($query) use ($dateFrom, $dateTo) {
-                $query->where('scheduleType', self::TYPE_REGULAR)
-                ->where(function($query) use ($dateFrom, $dateTo) {
-                    $query->whereBetween('startRecur', [$dateFrom, $dateTo])
-                    ->orWhere(function($query) use ($dateFrom, $dateTo) {
-                        $query->where('startRecur', '<=', $dateFrom)
-                        ->where(function($query) use ($dateTo) {
-                            $query->where('endRecur', '>=', $dateTo)
-                            ->orWhereNull('endRecur');
+        return $query->where(function ($query) use ($dateFrom, $dateTo) {
+            $query->where('scheduleType', self::TYPE_REGULAR)
+            ->where(function ($query) use ($dateFrom, $dateTo) {
+                $query->whereBetween('startRecur', [$dateFrom, $dateTo])
+                ->orWhereBetween('endRecur', [$dateFrom, $dateTo])
+                ->orWhere(function ($query) use ($dateFrom, $dateTo) {
+                    $query
+                    ->whereDate("startRecur", "<=", $dateFrom)
+                    ->whereDate("startRecur", "<=", $dateTo)
+                    ->where(function ($query) use ($dateFrom, $dateTo) {
+                        $query->whereNull('endRecur')
+                        ->orWhere(function ($query) use ($dateFrom, $dateTo) {
+                            $query->whereDate("endRecur", ">", $dateFrom)
+                            ->whereDate("endRecur", ">", $dateTo);
                         });
                     });
                 });
-            })->orWhere(function($query) use ($dateFrom, $dateTo) {
-                $query->where('scheduleType', self::TYPE_IRREGULAR)
-                ->whereBetween('startRecur', [$dateFrom, $dateTo]);
             });
+        })->orWhere(function($query) use ($dateFrom, $dateTo) {
+            $query->where('scheduleType', self::TYPE_IRREGULAR)
+            ->whereBetween('startRecur', [$dateFrom, $dateTo]);
         });
     }
 
