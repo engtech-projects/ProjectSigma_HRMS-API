35da98f7f2e27d628cf9e3a5ce0a59568ca9ba62
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2024-09-27 19:00:59 +0800

    Update Attendance.php

--- app/Http/Traits/Attendance.php
+++ app/Http/Traits/Attendance.php
@@ -144,13 +144,11 @@ trait Attendance
             $currentDuration = round($dtrIn->diffInMinutes($dtrOut) / 60, 2);
             $currentDuration = $currentDuration > 0 ? $currentDuration : 0;
             $duration += $currentDuration;
-            if ($charge) {
-                array_push($chargings, [
-                    "model" => get_class($charge),
-                    "id" => $charge->id,
-                    "hrs_worked" => $currentDuration,
-                ]);
-            }
+            array_push($chargings, [
+                "model" => $charge['class'] ?? Department::class,
+                "id" => $charge['id'] ?? 4,
+                "hrs_worked" => $currentDuration,
+            ]);
         }
 
         return [
