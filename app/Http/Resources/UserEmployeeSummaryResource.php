84337d67134d703092e9d66f90fd905b71ef7a26
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-10-11 16:04:16 +0800

    Update: added id & email

--- app/Http/Resources/UserEmployeeSummaryResource.php
+++ app/Http/Resources/UserEmployeeSummaryResource.php
@@ -15,6 +15,8 @@ class UserEmployeeSummaryResource extends JsonResource
     public function toArray(Request $request): array
     {
         return [
+            'id' => $this->id,
+            'email' => $this->email,
             "employee" => new EmployeeSummaryResource($this->employee),
         ];
     }
