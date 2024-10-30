99b6b9850ccd9f9575a165a76a7857ac9cf28181
mbunico <mark.unico@aclcbutuan.edu.ph>
2024-10-30 13:03:31 +0800

    Update AccessibilityInventory & seeder

--- app/Enums/AccessibilityInventory.php
+++ app/Enums/AccessibilityInventory.php
@@ -16,11 +16,10 @@ enum AccessibilityInventory: string
     case INVENTORY_ITEMPROFILE_NEW_FORM = "inventory:item profile_new profile_forms and my requests";
     case INVENTORY_WAREHOUSE_VIEWONLY = "inventory:warehouse_overview_view only";
     case INVENTORY_WAREHOUSE_PSSMANAGER = "inventory:warehouse_overview_pss manager";
-    case INVENTORY_REQUESTBOM_NEW_REQUEST = "inventory:request bom_new request";
-    case INVENTORY_REQUESTBOM_FORM = "inventory:request bom_forms and my requests";
-    case INVENTORY_REQUESTBOM_ALLREQUESTS = "inventory:request bom_list";
-    case INVENTORY_REQUESTBOM_MYAPPROVALS = "inventory:request bom_my approvals";
-    case INVENTORY_REQUESTBOM_CURRENTBOM = "inventory:request bom_current bom";
+    case INVENTORY_BOM_FORM = "inventory:bom_form and my requests";
+    case INVENTORY_BOM_ALLREQUESTS = "inventory:bom_all requests";
+    case INVENTORY_BOM_MYAPPROVALS = "inventory:bom_my approvals";
+    case INVENTORY_BOM_CURRENTBOM = "inventory:bom_current bom";
 
     public static function toArray(): array
     {
