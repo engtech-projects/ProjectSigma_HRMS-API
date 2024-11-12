b183c862eaa79744723c0dd4c1c6f04ea56fae48
Rustom Pedales <pedales.rustom@gmail.com>
2024-11-12 12:53:33 +0800

    [Update]: stablish accounting accessibility

--- app/Enums/AccessibilityAccounting.php
+++ app/Enums/AccessibilityAccounting.php
@@ -5,16 +5,29 @@ namespace App\Enums;
 enum AccessibilityAccounting: string
 {
     case ACCOUNTING_DASHBOARD = "accounting:dashboard";
-    // case ACCOUNTING_ = "accounting:chart of accounts";
-    // case ACCOUNTING_ = "accounting:books";
-    // case ACCOUNTING_ = "accounting:transaction type";
-    // case ACCOUNTING_ = "accounting:document Series";
-    // case ACCOUNTING_ = "accounting:posting period";
-    // case ACCOUNTING_ = "accounting:account groups";
-    // case ACCOUNTING_ = "accounting:stake holder";
-    case ACCOUNTING_JOURNALENTRY_VIEWALL = "accounting:journal entry_view all entries";
-    case ACCOUNTING_JOURNALENTRY_CREATE = "accounting:journal entry_create";
-    case ACCOUNTING_JOURNALENTRY_MODIFY = "accounting:journal entry_modify entry";
+
+    //Accounting Setup
+    case ACCOUNTING_SETUP_APPROVALS = "accounting:setup_approvals";
+    case ACCOUNTING_SETUP_ACCOUNTS = "accounting:setup_accounts";
+    case ACCOUNTING_SETUP_BOOK_OF_ACCOUNTS = "accounting:setup_book of accounts";
+    case ACCOUNTING_SETUP_ACCOUNT_GROUPS = "accounting:setup_account groups";
+    case ACCOUNTING_SETUP_ACCOUNT_TYPES = "accounting:setup_account types";
+    case ACCOUNTING_SETUP_POSTING_PERIODS = "accounting:setup_posting periods";
+    case ACCOUNTING_SETUP_CHART_OF_ACCOUNTS = "accounting:setup_chart of accounts";
+    case ACCOUNTING_SETUP_STAKEHOLDERS = "accounting:setup_stakeholders";
+    case ACCOUNTING_SETUP_SYNCHRONIZATION = "accounting:setup_synchronization";
+
+    //Accounting Request
+    case ACCOUNTING_REQUEST_PURCHASE_ORDER = "accounting:request_purchase order";
+    case ACCOUNTING_REQUEST_NON_PURCHASE_ORDER = "accounting:request_non purchase order";
+    case ACCOUNTING_REQUEST_PRE_PAYROLL_AUDIT = "accounting:request_pre payroll audit";
+
+    //Accounting Voucher
+    case ACCOUNTING_VOUCHER_DISBURSEMENT = "accounting:voucher_disbursement";
+    case ACCOUNTING_VOUCHER_CASH = "accounting:voucher_cash";
+
+    //Accounting Journal
+    case ACCOUNTING_JOURNAL_ENTRY = "accounting:journal_journal entry";
 
     public static function toArray(): array
     {
