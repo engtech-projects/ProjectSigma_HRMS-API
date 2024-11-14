d5da3194f1d2f9b825423048bb167b8ff38ca463
Jose Janlofre Dy <janlofredy@gmail.com>
2024-11-14 13:16:14 +0800

    Update AttendanceService.php

--- app/Http/Services/Attendance/AttendanceService.php
+++ app/Http/Services/Attendance/AttendanceService.php
@@ -315,7 +315,7 @@ class AttendanceService
             }
         } elseif (Self::checkHasHoliday($employeeDayData["events"], EventTypes::SPECIALHOLIDAY->value, 1)) { // Special Holiday
             $type = "special_holidays";
-            if (Self::checkHasHoliday($employeeDayData["events"], EventTypes::REGULARHOLIDAY->value, 1, 0)) {
+            if (Self::checkHasHoliday($employeeDayData["events"], EventTypes::SPECIALHOLIDAY->value, 1, 0)) {
                 // WITHOUT WORK
                 // TOTAL DAY SCHEDULE - WORK RENDERED = regular
                 $metaResult["regular"]["reg_hrs"] += $daySchedulesDuration - $workRendered["rendered"];
