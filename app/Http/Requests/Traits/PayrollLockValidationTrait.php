b856fa67352a13d434a8e85a4f3c0ea584122803
Jose Janlofre Dy <janlofredy@gmail.com>
2024-12-05 14:16:03 +0800

    fix payroll date lock checker

--- app/Http/Requests/Traits/PayrollLockValidationTrait.php
+++ app/Http/Requests/Traits/PayrollLockValidationTrait.php
@@ -10,7 +10,7 @@ trait PayrollLockValidationTrait
 {
     public function isPayrollLocked($dateCheck)
     {
-        Carbon::parse($dateCheck)
+        $dateCheck = Carbon::parse($dateCheck);
         $allSettings = Settings::get();
         $pr1Day = $allSettings->where('setting_name', AttendanceSettings::PAYROLL_20TH_LOCKUP_DAY_LIMIT->value)->first()->value;
         $pr1SchedDay = $allSettings->where('setting_name', AttendanceSettings::PAYROLL_20TH_LOCKUP_SCHEDULE_DAY_OF_MONTH->value)->first()->value;
@@ -19,24 +19,24 @@ trait PayrollLockValidationTrait
         $pr2SchedDay = $allSettings->where('setting_name', AttendanceSettings::PAYROLL_5TH_LOCKUP_SCHEDULE_DAY_OF_MONTH->value)->first()->value;
         $pr2SchedTime = $allSettings->where('setting_name', AttendanceSettings::PAYROLL_5TH_LOCKUP_SCHEDULE_TIME_OF_DAY->value)->first()->value;
         $dateToday = Carbon::now();
-        $pr1Sched1 = $dateToday->copy()->subMonth()->setDay($pr1SchedDay)->setTime($pr1SchedTime);
+        $pr1Sched1 = $dateToday->copy()->subMonth()->setDay($pr1SchedDay)->setTimeFromTimeString($pr1SchedTime);
         $pr1Date1 = $dateToday->copy()->subMonth()->setDay($pr1Day);
-        $pr1Sched2 = $dateToday->copy()->setDay($pr1SchedDay)->setTime($pr1SchedTime);
+        $pr1Sched2 = $dateToday->copy()->setDay($pr1SchedDay)->setTimeFromTimeString($pr1SchedTime);
         $pr1Date2 = $dateToday->copy()->setDay($pr1Day);
-        $pr2Sched1 = $dateToday->copy()->subMonth()->setDay($pr2SchedDay)->setTime($pr2SchedTime);
+        $pr2Sched1 = $dateToday->copy()->subMonth()->setDay($pr2SchedDay)->setTimeFromTimeString($pr2SchedTime);
         $pr2Date1 = $dateToday->copy()->subMonth()->setDay($pr2Day);
-        $pr2Sched2 = $dateToday->copy()->setDay($pr2SchedDay)->setTime($pr2SchedTime);
+        $pr2Sched2 = $dateToday->copy()->setDay($pr2SchedDay)->setTimeFromTimeString($pr2SchedTime);
         $pr2Date2 = $dateToday->copy()->setDay($pr2Day);
-        $maxDateAllowed =
+        $maxDateAllowed = Carbon::now()->endOfMonth();
         if ($dateToday->lt($pr1Sched1)) {
-
+            $maxDateAllowed = $pr1Date1;
         } else if ($dateToday->lt($pr2Sched1)) {
-
+            $maxDateAllowed = $pr2Date1;
         } else if ($dateToday->lt($pr1Sched2)) {
-
+            $maxDateAllowed = $pr1Date2;
         } else if ($dateToday->lt($pr2Sched2)) {
-
+            $maxDateAllowed = $pr2Date2;
         }
-
+        return $maxDateAllowed->lt($dateCheck);
     }
 }
