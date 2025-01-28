1f88fbd8bd011da086b71c78af08d3b75dd0d7a5
Jose Janlofre Dy <janlofredy@gmail.com>
2025-01-28 11:31:44 +0800

    Update HrmsServiceApprovalAttributeResource.php

--- app/Http/Resources/HrmsServiceApprovalAttributeResource.php
+++ app/Http/Resources/HrmsServiceApprovalAttributeResource.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Resources;
 
+use App\Enums\ApprovalStatus;
 use App\Models\Users;
 use Carbon\Carbon;
 use Illuminate\Http\Request;
@@ -16,21 +17,21 @@ class HrmsServiceApprovalAttributeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $user = Users::with('employee')->where('id', $this["user_id"])->first();
-        $employee = null;
-        if ($user) {
-            $employee = $user->employee ? new EmployeeUserResource($user->employee) : null;
-        }
+        $user = Users::with('employee')->find($this["user_id"]);
         return [
             "type" => $this["type"],
             "status" => $this["status"] ?? null,
+            "selector_type" => $this["selector_type"] ?? null,
             "user_id" => $this["user_id"] ?? null,
             "remarks" => $this["remarks"] ?? null,
             "date_approved" => $this["date_approved"] ?? null,
             "date_approved_human" => ($this["date_approved"] ?? null) ? Carbon::parse($this["date_approved"])->format('F j, Y h:i A') : null,
             "date_denied" => $this["date_denied"] ?? null,
             "date_denied_human" => ($this["date_denied"] ?? null) ? Carbon::parse($this["date_denied"])->format('F j, Y h:i A') : null,
-            "employee" => $employee
+            "employee_name" => $user?->employee?->fullname_first ?? "SYSTEM ADMINISTRATOR",
+            "employee_position" => $user?->employee?->current_position_name ?? "SYSTEM ADMINISTRATOR",
+            "user_name" => $user?->name ?? "SYSTEM ADMINISTRATOR",
+            "employee_signature" => ($this["status"] ?? null) === ApprovalStatus::APPROVED->value ? $user?->employee?->digital_signature?->base64 : null
         ];
     }
 }
