3b8198facd8cb6055560e9331dd2e9dd68281bb2
Jose Janlofre Dy <janlofredy@gmail.com>
2025-01-28 10:59:48 +0800

    Update ApprovalAttributeResource.php

--- app/Http/Resources/ApprovalAttributeResource.php
+++ app/Http/Resources/ApprovalAttributeResource.php
@@ -17,7 +17,7 @@ class ApprovalAttributeResource extends JsonResource
      */
     public function toArray(Request $request): array
     {
-        $user = Users::with('employee')->where('id', $this["user_id"])->first();
+        $user = Users::with('employee')->find($this["user_id"]);
         return [
             "type" => $this["type"],
             "status" => $this["status"] ?? null,
@@ -28,10 +28,10 @@ class ApprovalAttributeResource extends JsonResource
             "date_approved_human" => ($this["date_approved"] ?? null) ? Carbon::parse($this["date_approved"])->format('F j, Y h:i A') : null,
             "date_denied" => $this["date_denied"] ?? null,
             "date_denied_human" => ($this["date_denied"] ?? null) ? Carbon::parse($this["date_denied"])->format('F j, Y h:i A') : null,
-            "employee_name" => $user->employee?->fullname_first ?? "SYSTEM ADMINISTRATOR",
-            "employee_position" => $user->employee?->current_position_name ?? "SYSTEM ADMINISTRATOR",
-            "user_name" => $user->name,
-            "employee_signature" => $this["status"] === ApprovalStatus::APPROVED->value ? $user->employee?->digital_signature->base64 : null
+            "employee_name" => $user?->employee?->fullname_first ?? "SYSTEM ADMINISTRATOR",
+            "employee_position" => $user?->employee?->current_position_name ?? "SYSTEM ADMINISTRATOR",
+            "user_name" => $user?->name ?? "SYSTEM ADMINISTRATOR",
+            "employee_signature" => ($this["status"] ?? null) === ApprovalStatus::APPROVED->value ? $user?->employee?->digital_signature?->base64 : null
         ];
     }
 }
