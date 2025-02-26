d98bdd0e580b783b5f630f56dde64b4f3b4634b2
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-02-26 11:13:53 +0800

    Update LateAbsenceController.php

--- app/Http/Controllers/LateAbsenceController.php
+++ app/Http/Controllers/LateAbsenceController.php
@@ -10,7 +10,7 @@ use Illuminate\Support\Facades\Cache;
 
 class LateAbsenceController extends Controller
 {
-    public function getLateAbsenceThisMonth()
+    public function getLateAbsenceThisMonth(Request $request)
     {
         $startOfMonth = Carbon::now()->startOfMonth();
         $endOfMonth = Carbon::now();
@@ -20,31 +20,36 @@ class LateAbsenceController extends Controller
             "date_from" => $startOfMonth,
             "date_to" => $endOfMonth,
         ];
-
         $cacheKey = 'employee_late_absences_' . $startOfMonth->format('Y_m') . '_' . $endOfMonth->format('Y_m');
 
         $reportData = Cache::remember($cacheKey, 1440, function() use ($filter) {
             return ReportService::employeeAbsences($filter);
         });
+        if ($request->has('reload') && $request->input('reload') === "true") {
+            $reportData = ReportService::employeeAbsences($filter);
+            Cache::put($cacheKey, $reportData, 1440);
+        }
 
         $newData = [
             'lates' => collect(),
             'absence' => collect()
         ];
 
-        $reportData->each(function($data) use (&$newData) {
-            if ($data["total_lates"] > 0) {
-                $newData["lates"]->push($data);
-            }
-            if ($data["total_absents"] > 0) {
-                $newData["absence"]->push($data);
-            }
-        });
+        if (count($reportData) > 0) {
+            $reportData->each(function($data) use (&$newData) {
+                if ($data["total_lates"] > 0) {
+                    $newData["lates"]->push($data);
+                }
+                if ($data["total_absents"] > 0) {
+                    $newData["absence"]->push($data);
+                }
+            });
+        }
 
         return new JsonResponse([
             "success" => true,
             "message" => "Successfully fetched.",
-            "data" => $newData
+            "data" => $newData,
         ]);
     }
 }
