951e62bad3c0d5296230f8b1f252e75408c815b5
Jose Janlofre Dy <35553424+janlofredy@users.noreply.github.com>
2025-04-14 13:46:33 +0800

    Update DatabaseSeeder.php

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -24,11 +24,5 @@ class DatabaseSeeder extends Seeder
                 ApprovalsSeeder::class
             ]
         );
-        // \App\Models\User::factory(10)->create();
-
-        // \App\Models\User::factory()->create([
-        //     'name' => 'Test User',
-        //     'email' => 'test@example.com',
-        // ]);
     }
 }
