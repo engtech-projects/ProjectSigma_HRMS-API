a2d5238d64456b9ca3d20013a4f7bb40e4b4d2cf
mbunico <mark.unico@aclcbutuan.edu.ph>
2025-04-28 16:06:52 +0800

    Updated accessibility

--- app/Enums/AccessibilityInventory.php
+++ app/Enums/AccessibilityInventory.php
@@ -36,9 +36,9 @@ enum AccessibilityInventory: string
     case INVENTORY_REQUESTSTOCKS_FORM = "inventory:request stock_form and my requests";
     case INVENTORY_REQUESTSTOCKS_ALLREQUESTS = "inventory:request stock_all requests";
     case INVENTORY_REQUESTSTOCKS_MYAPPROVALS = "inventory:request stock_my approvals";
-    case INVENTORY_PROCUREMENT_PRICEQUOTATION_REQUESTS = "inventory:procurement_price quotation_requests";
-    case INVENTORY_PROCUREMENT_PRICEQUOTATION_ALLQUOTATIONS = "inventory:procurement_price quotation_all quotations";
-    case INVENTORY_PROCUREMENT_PRICEQUOTATION_MYQUOTATIONS = "inventory:procurement_price quotation_my quotations";
+    case INVENTORY_PROCUREMENT_REQUESTPRICEQUOTATION_REQUESTS = "inventory:procurement_request price quotation_requests";
+    case INVENTORY_PROCUREMENT_REQUESTPRICEQUOTATION_ALLQUOTATIONS = "inventory:procurement_request price quotation_all quotations";
+    case INVENTORY_PROCUREMENT_REQUESTPRICEQUOTATION_MYQUOTATIONS = "inventory:procurement_request price quotation_my quotations";
     public static function toArray(): array
     {
         $array = [];
