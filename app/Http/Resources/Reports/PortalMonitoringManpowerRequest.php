544736a1844bf0cdfa23a979538926156c5fbc91
DanielDoysabas <124086485+DanielDoysabas@users.noreply.github.com>
2025-05-06 14:37:50 +0800

    Update PortalMonitoringManpowerRequest.php

--- app/Http/Resources/Reports/PortalMonitoringManpowerRequest.php
+++ app/Http/Resources/Reports/PortalMonitoringManpowerRequest.php
@@ -13,26 +13,26 @@ class PortalMonitoringManpowerRequest extends JsonResource
      * @return array<string, mixed>
      */
 
-     public function toArray(Request $request): array
-     {
-         $approvals = $this->summary_approvals;
-         return [
-             "requesting_department" => $this['department']->department_name,
-             "requested_position" => $this['position']->name,
-             "employment_type" => $this['employment_type'],
-             "nature_of_request" => $this['nature_of_request'],
-             "age_range" => $this['age_range'],
-             "civil_status" => $this["status"],
-             "gender" => $this["gender"],
-             "education_requirement" => $this["educational_requirement"],
-             "preffered_qualification" => $this["preferred_qualifications"],
-             "date_required" => $this->date_required_human,
-             "date_requested" => $this->date_requested_human,
-             "requested_by" => $this->created_by_user_name,
-             "request_status" => $this['request_status'],
-             "days_delayed_filling" => $this->days_delayed_filing,
-             "date_approved" => $this->date_approved_date_human,
-             "approvals" => $approvals
-         ];
-     }
+    public function toArray(Request $request): array
+    {
+        $approvals = $this->summary_approvals;
+        return [
+            "requesting_department" => $this['department']->department_name,
+            "requested_position" => $this['position']->name,
+            "employment_type" => $this['employment_type'],
+            "nature_of_request" => $this['nature_of_request'],
+            "age_range" => $this['age_range'],
+            "civil_status" => $this["status"],
+            "gender" => $this["gender"],
+            "education_requirement" => $this["educational_requirement"],
+            "preffered_qualification" => $this["preferred_qualifications"],
+            "date_required" => $this->date_required_human,
+            "date_requested" => $this->date_requested_human,
+            "requested_by" => $this->created_by_user_name,
+            "request_status" => $this['request_status'],
+            "days_delayed_filling" => $this->days_delayed_filing,
+            "date_approved" => $this->date_approved_date_human,
+            "approvals" => $approvals
+        ];
+    }
 }
