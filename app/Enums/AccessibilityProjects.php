6cc0c9b7ecbe146187faf4eca5921bcbbdc30590
J <115533073+jbanban@users.noreply.github.com>
2025-06-25 13:19:48 +0800

    project accessibility adjusments

--- app/Enums/AccessibilityProjects.php
+++ app/Enums/AccessibilityProjects.php
@@ -7,15 +7,16 @@ enum AccessibilityProjects: string
     case PROJECTMONITORING_DASHBOARD = "project monitoring:dashboard";
     case PROJECTMONITORING_PROJECT = "project monitoring:projects";
     case PROJECTMONITORING_MARKETING = "project monitoring:marketing";
-    case PROJECT_MONITORING_MARKETING_MY_PROJECTS = "project monitoring:marketing_my projects";
-    case PROJECT_MONITORING_MARKETING_BIDDING_LIST = "project monitoring:marketing_bidding list";
-    case PROJECT_MONITORING_MARKETING_PROPOSAL_LIST = "project monitoring:marketing_proposal list";
-    case PROJECT_MONITORING_MARKETING_ARCHIVED_LIST = "project monitoring:marketing_archived list";
-    case PROJECT_MONITORING_MARKETING_ON_HOLD_LIST = "project monitoring:marketing_on hold list";
-    case PROJECT_MONITORING_MARKETING_AWARDED_LIST = "project monitoring:marketing_awarded list";
-    case PROJECT_MONITORING_MARKETING_DRAFT_LIST = "project monitoring:marketing_draft list";
+    case PROJECTMONITORING_MARKETING_MY_PROJECTS = "project monitoring:marketing_my projects";
+    case PROJECTMONITORING_MARKETING_BIDDING_LIST = "project monitoring:marketing_bidding list";
+    case PROJECTMONITORING_MARKETING_PROPOSAL_LIST = "project monitoring:marketing_proposal list";
+    case PROJECTMONITORING_MARKETING_ARCHIVED_LIST = "project monitoring:marketing_archived list";
+    case PROJECTMONITORING_MARKETING_ON_HOLD_LIST = "project monitoring:marketing_on hold list";
+    case PROJECTMONITORING_MARKETING_AWARDED_LIST = "project monitoring:marketing_awarded list";
+    case PROJECTMONITORING_MARKETING_DRAFT_LIST = "project monitoring:marketing_draft list";
     case PROJECTMONITORING_TSS = "project monitoring:tss";
-    case PROJECTMONITORING_SETUP = "project monitoring:setup";
+    case PROJECTMONITORING_SETUP_APPROVALS = "project monitoring:setup_approvals";
+    case PROJECTMONITORING_SETUP_POSITION = "project monitoring:setup_position";
     case PROJECTMONITORING_SETUP_SYNCHRONIZATION = "project monitoring:setup_synchronization";
 
     public static function toArray(): array
