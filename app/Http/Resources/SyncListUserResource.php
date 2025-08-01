3c835ff24439dd3648e0e63a2985e9172fdc3179
Jose Janlofre Dy <janlofredy@gmail.com>
2025-08-01 18:36:50 +0800

    fix sync lists to use Resource and include deleted data so it will update other api's lists when deleted

---
+++ app/Http/Resources/SyncListUserResource.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Resources;
+
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+class SyncListUserResource extends JsonResource
+{
+    /**
+     * Transform the resource into an array.
+     *
+     * @return array<string, mixed>
+     */
+    public function toArray(Request $request): array
+    {
+        return [
+            'id' => $this->id,
+            'name' => $this->name,
+            'email' => $this->email,
+            'email_verified_at' => $this->email_verified_at,
+            'password' => $this->password,
+            'remember_token' => $this->remember_token,
+            'type' => $this->type,
+            'accessibilities' => is_array($this->accessibilities) ? json_encode($this->accessibilities) : (string)$this->accessibilities,
+            'employee_id' => $this->employee_id,
+            'created_at' => $this->created_at?->format('Y-m-d H:i:s'),
+            'updated_at' => $this->updated_at?->format('Y-m-d H:i:s'),
+            'deleted_at' => $this->deleted_at?->format('Y-m-d H:i:s'),
+        ];
+    }
+}
