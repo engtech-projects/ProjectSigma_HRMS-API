21145f34cd209f30fa00865ef344a62d89b7945e
Jose Janlofre Dy <janlofredy@gmail.com>
2025-08-15 18:19:06 +0800

    fix pint

--- app/Models/Overtime.php
+++ app/Models/Overtime.php
@@ -9,7 +9,6 @@ use App\Traits\HasApproval;
 use App\Traits\ModelHelpers;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\HasOne;
 use Illuminate\Database\Eloquent\SoftDeletes;
 use Illuminate\Database\Eloquent\Builder;
--- app/Models/TravelOrder.php
+++ app/Models/TravelOrder.php
@@ -10,7 +10,6 @@ use Carbon\Carbon;
 use Laravel\Sanctum\HasApiTokens;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
-use Illuminate\Database\Eloquent\Relations\BelongsTo;
 use Illuminate\Database\Eloquent\Relations\BelongsToMany;
 use Illuminate\Database\Eloquent\Relations\HasMany;
 use Illuminate\Database\Eloquent\Relations\HasOne;
