20f764c445228369e9f517b46edd182d7a694bef
eptals1 <eptals2@gmail.com>
2025-09-05 13:48:33 +0800

    Move project approvals seeding to separate class
    Extracted project-related approvals seeding logic from ApprovalsSeeder.php into a new ApprovalsSeederProject.php class for better separation of concerns. Updated DatabaseSeeder.php to include the new seeder.

--- database/seeders/ApprovalsSeeder.php
+++ database/seeders/ApprovalsSeeder.php
@@ -270,63 +270,6 @@ class ApprovalsSeeder extends Seeder
                 'deleted_at',
             ]
         );
-        // PROJECTS
-        DB::table('approvals')->upsert(
-            [
-                [
-                    'id' => 3010,
-                    'form' => "Project Award Request",
-                    'approvals' => "[]",
-                    'module' => ApprovalModules::PROJECT->value,
-                    'deleted_at' => null,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ],
-                [
-                    'id' => 3020,
-                    'form' => "Schedule for Submission",
-                    'approvals' => "[]",
-                    'module' => ApprovalModules::PROJECT->value,
-                    'deleted_at' => null,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ],
-                [
-                    'id' => 3030,
-                    'form' => "Bill of Materials",
-                    'approvals' => "[]",
-                    'module' => ApprovalModules::PROJECT->value,
-                    'deleted_at' => null,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ],
-                [
-                    'id' => 3040,
-                    'form' => "Direct Cost",
-                    'approvals' => "[]",
-                    'module' => ApprovalModules::PROJECT->value,
-                    'deleted_at' => null,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ],
-                [
-                    'id' => 3050,
-                    'form' => "Detailed Estimates",
-                    'approvals' => "[]",
-                    'module' => ApprovalModules::PROJECT->value,
-                    'deleted_at' => null,
-                    'created_at' => Carbon::now(),
-                    'updated_at' => Carbon::now(),
-                ],
-            ],
-            [
-                'id'
-            ],
-            [
-                'form',
-                'module',
-                'deleted_at',
-            ]
-        );
+
     }
 }
