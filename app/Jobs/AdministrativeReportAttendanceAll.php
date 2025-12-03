df8ba3f8954a1d621947f5238dc3cbf3e992de2f
Jose Janlofre Dy <janlofredy@gmail.com>
2025-12-03 15:32:33 +0800

    Update AdministrativeReportAttendanceAll.php

--- app/Jobs/AdministrativeReportAttendanceAll.php
+++ app/Jobs/AdministrativeReportAttendanceAll.php
@@ -33,7 +33,7 @@ class AdministrativeReportAttendanceAll implements ShouldQueue, ShouldBeUnique
         $reportData = ReportService::employeeAbsences($this->validated);
         // STORE REPORT DATA IN FILE IN CACHE AND SCHEDULE DELETION
         $cacheKey = $this->validated['group_type'] . '-' . $this->validated['group_type'] . '-' . $this->validated['date_from'] . '-' . $this->validated['date_to'];
-        $path = 'public/reports/'. $cacheKey . '.json';
+        $path = 'storage/temp-report-generations/'. $cacheKey . '.json';
         $storedPath = Storage::put($path, json_encode($reportData));
         DeleteTempFileAfterDelay::dispatch($storedPath)->delay(now()->addDay());
         cache()->put($cacheKey, $storedPath, now()->addDay());
